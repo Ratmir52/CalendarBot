8c180bacf516b2ca279351765cf5c1f554b22c3f
Ratmir52 <romanoffratmir@gmail.com>
2025-11-24 14:59:41 +0300

    Update GoogeParse.py

--- api/GoogeParse.py
+++ api/GoogeParse.py
@@ -1,4 +1,4 @@
-# Libraries
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -8,7 +8,7 @@ import datetime
 from database import db
 from dateutil import parser
 
-# Parce Events for GoogleAPI with user auch_token and refresh_token.
+
 def ParseEvents(creds, id, chat_id):
 
     service = build("calendar", "v3", credentials=creds)
@@ -32,17 +32,17 @@ def ParseEvents(creds, id, chat_id):
     if not events:
         return
 
-# Prints the start and name of the next 10 events
     for event in events:
-        # Get event time from GoogleAPI
+
         iso_time = event["start"].get("dateTime", event["start"].get("date"))
         dt = parser.isoparse(iso_time)
-        # Translate ISO to UNIX time format.
+
         start_unix = int(dt.timestamp())
         summary = event.get("summary", "Без названия")
 
         print(f"Timer {summary} дата unix: {start_unix}")
 
         db.add_reminder(id, chat_id, summary, start_unix)
+
 
 
